64660dfee3ef3cc1cd7f3b01f93758718259ac7e
0s1n <jpablo_166@hotmail.com>
2024-09-30 13:54:09 -0500

    fix: added lusca #28

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
+const lusca = require("lusca");
 const { logger, logEvents } = require("./middleware/logger");
 const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
+
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const connectDB = require("./config/dbConn");
@@ -21,6 +23,8 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(lusca.csrf());
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
